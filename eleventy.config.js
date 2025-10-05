42cdc2f900a47790e6b5937d3a985b35e1c5f470
sloep <113307853+sloep@users.noreply.github.com>
2025-10-05 20:14:18 +0200

    add time to read and post date formatting

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,30 @@ export default function (eleventyConfig) {
 	// Passthrough
 	eleventyConfig.addPassthroughCopy({ "src/assets": "." });
 
+	// Filters
+	eleventyConfig.addFilter("timeToRead", (content) => {
+		const getPlainText = (html) => {
+			const htmlTags = String.raw`<\\/?[ a-z0-9]+\\b[^>]*>`;
+			const htmlComments = String.raw`<!--[ ^]*?-->`;
+			return html.replace(new RegExp(String.raw`${htmlTags}|${htmlComments}`, "gi"), "");
+		};
+
+		const rawText = getPlainText(content);
+		const wpm = 240;
+		const words = rawText.trim().split(/\s+/).length;
+		const time = Math.ceil(words / wpm);
+
+		return time;
+	});
+	eleventyConfig.addFilter("postDate", (date) => {
+		return new Date(date).toLocaleDateString("nl-NL", {
+			day: "numeric",
+			month: "long",
+			year: "numeric",
+		});
+	});
+	
+
 	return {
 		dir: {
 			input: "src/pages",
